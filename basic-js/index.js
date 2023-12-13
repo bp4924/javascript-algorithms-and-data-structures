76bd24e7c0f9746a30ba7f54f3d3dc5eb0b9fb7e
Bernie Petters <bp4924@gmail.com>
2023-12-13 12:06:13 -0500

    initial commit

---
+++ basic-js/index.js
@@ -0,0 +1,32 @@
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a = a + 12;
+b = 9 + b;
+c = c + 7;
+
+// +=, -=, *=, /=
+// escape character \ for literal quotes
+const myStr = 'I am a "double quoted" string inside "double quotes"';
+
+const myName = "Bernie";
+const myIntro = "Hello. I am " + myName + " How are you?";
+
+console.log(myIntro);
+
+// use variables to complete a sentence
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks =
+  "My " + myAdjective + " " + myNoun + " " + myAdverb + " " + myVerb + " away."; // Change this line
+// Only change code above this line
+
+console.log(wordBlanks);
+
+// multiple values
