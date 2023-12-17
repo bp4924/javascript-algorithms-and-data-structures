27cd8ce3f6e69fd418a42dd6a0893bebd45053ab
Bernie Petters <bp4924@gmail.com>
2023-12-17 10:55:17 -0500

    modify record collection

--- basic-js/index.js
+++ basic-js/index.js
@@ -288,7 +288,7 @@ console.log(cc("A"));
 // Setup
 function phoneticLookup(val) {
   let result = "";
-  console.log(val);
+  //  console.log(val);
   // Only change code below this line
   const lookup = {
     alpha: "Adams",
@@ -305,4 +305,62 @@ function phoneticLookup(val) {
   return result;
 }
 
-console.log(phoneticLookup("bravo"));
+//console.log(phoneticLookup("bravo"));
+
+// accessing nested arrays
+const myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
+  },
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
+];
+
+const secondTree = myPlants[1].list[1];
+// console.log(secondTree);
+
+// -- Record Collection --
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (value === "") {
+    delete records[id][prop];
+  } else if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && value !== "") {
+    if (records[id].hasOwnProperty("tracks") == false) {
+      records[id][prop] = [];
+    }
+    records[id][prop].push(value);
+  }
+  return records;
+}
+
+console.log(updateRecords(recordCollection, 5439, "artist", "ABBA"));
+
+console.log(
+  updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me")
+);
