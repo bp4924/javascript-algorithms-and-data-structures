ee1707337b5a4096077d0bd659a69c4286835802
Bernie Petters <bp4924@gmail.com>
2023-12-21 10:07:52 -0500

    contact list

--- basic-js/index.js
+++ basic-js/index.js
@@ -410,3 +410,78 @@ console.log(
 );
 
 // do ... while loop
+
+// recursive function
+function sum(arr, n) {
+  // Only change code below this line
+  if (n <= 0) {
+    return 1;
+  } else {
+    return sum(arr, n - 1) + arr[n - 1];
+  }
+  // Only change code above this line
+}
+
+// profile lookup
+// Setup
+const contacts = [
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+console.log(contacts.length);
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+
+  for (let x = 0; x < contacts.length; x++) {
+    if (contacts[x].firstName === name) {
+      if (contacts[x].hasOwnProperty(prop)) {
+        return contacts[x][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact " + name;
+
+  /*
+   for (let x = 0; x < contacts.length; x++) {
+    if (contacts[x].firstName === name) {
+      if (contacts[x].hasOwnProperty(prop)) {
+        return contacts[x][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+  */
+  // Only change code above this line
+}
+
+console.log(lookUpProfile("Akira", "lastName"));
+console.log(lookUpProfile("Kristian", "lastName"));
+console.log(lookUpProfile("Sherlock", "likes"));
+console.log(lookUpProfile("Harry", "likes"));
