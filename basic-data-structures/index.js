98360c526ea5c126a560fa078a51e6eea38a1f22
Bernie Petters <bp4924@gmail.com>
2024-01-12 10:00:13 -0500

    modify array in object

--- basic-data-structures/index.js
+++ basic-data-structures/index.js
@@ -251,11 +251,33 @@ let users = {
     online: true,
   },
 };
-
 function getArrayOfUsers(obj) {
-  // Only change code below this line
   return Object.keys(obj);
+}
+//console.log(getArrayOfUsers(users));
+
+// modify array in object
+let user = {
+  name: "Kenneth",
+  age: 28,
+  data: {
+    username: "kennethCodesAllDay",
+    joinDate: "March 26, 2016",
+    organization: "freeCodeCamp",
+    friends: ["Sam", "Kira", "Tomo"],
+    location: {
+      city: "San Francisco",
+      state: "CA",
+      country: "USA",
+    },
+  },
+};
+
+function addFriend(userObj, friend) {
+  // Only change code below this line
+  userObj.data.friends.push(friend);
+  return userObj.data.friends;
   // Only change code above this line
 }
 
-console.log(getArrayOfUsers(users));
+console.log(addFriend(user, "Pete"));
