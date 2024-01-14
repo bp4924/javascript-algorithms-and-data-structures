d476f5976282e8f44385dae98fd1b9f95ed46788
Bernie Petters <bp4924@gmail.com>
2024-01-14 09:29:55 -0500

    split array

--- basic-algorithm-scripting/index.js
+++ basic-algorithm-scripting/index.js
@@ -1,3 +1,7 @@
+const {
+  NetworkAccessProfileNetworkListInstance,
+} = require("twilio/lib/rest/supersim/v1/networkAccessProfile/networkAccessProfileNetwork");
+
 //convert celcius to fahrenheit
 let fahrenheit = 0;
 function convertCtoF(celsius) {
@@ -103,5 +107,109 @@ function truncateString(str, num) {
   }
   return newStr;
 }
+//console.log(truncateString("A-tisket a-tasket A green and yellow basket", 0));
 
-console.log(truncateString("A-tisket a-tasket A green and yellow basket", 0));
+// finders keepers
+function findElement(arr, func) {
+  let num = 0;
+  for (i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
+  }
+  return undefined;
+}
+
+//console.log(findElement([1, 5, 3, 7], (num) => num % 2 === 0));
+
+// boolean test
+function booWho(bool) {
+  if ((bool === true) | (bool === false)) {
+    return true;
+  }
+  return false;
+}
+//console.log(booWho(0));
+
+// title case
+function titleCase(str) {
+  const newTitle = str.split(" ");
+  const updatedTitle = [];
+  for (let st in newTitle) {
+    updatedTitle[st] =
+      newTitle[st][0].toUpperCase() + newTitle[st].slice(1).toLowerCase();
+  }
+  return updatedTitle.join(" ");
+}
+//console.log(titleCase("I'm a little tea pot"));
+
+// slice and splice
+function frankenSplice(arr1, arr2, n) {
+  let localArr = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    localArr.splice(n, 0, arr1[i]);
+    n++;
+  }
+
+  return localArr;
+}
+//console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+
+// falsy
+function bouncer(arr) {
+  const filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+      filteredArr.push(arr[i]);
+    }
+  }
+  return filteredArr;
+}
+// console.log(bouncer([7, "ate", "", false, 9]));
+
+// find index
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num) {
+      return i;
+    }
+  }
+  return arr.length;
+}
+//console.log(getIndexToIns([10, 20, 30, 40, 50], 30));
+
+// mutations
+function mutation(arr) {
+  let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) {
+      return false;
+    }
+  }
+  return true;
+}
+// console.log(mutation(["hello", "heLlllo"]));
+
+// chunk - split array into smaller arrays
+function chunkArrayInGroups(arr, size) {
+  let result = [];
+  let temp = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % size !== size - 1) {
+      temp.push(arr[i]);
+    } else {
+      temp.push(arr[i]);
+      result.push(temp);
+      temp = [];
+    }
+  }
+  if (temp.length !== 0) {
+    result.push(temp);
+  }
+  return result;
+}
+
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 3));
