e9af50aa17a1807bba1febb8804ade8246ae8735
Bernie Petters <bp4924@gmail.com>
2024-02-07 07:47:48 -0500

    insert spaces

--- intermediate-algorithm-scripting/zig.js
+++ intermediate-algorithm-scripting/zig.js
@@ -1,3 +1,5 @@
+// LEETCODE Medium # 6
+
 //The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
 /*
 P   A   H   N
@@ -15,4 +17,64 @@ string convert(string s, int numRows);
  * @param {number} numRows
  * @return {string}
  */
-var convert = function (s, numRows) {};
+var convert = function (s, numRows) {
+  // split s
+  const splitS = s.split("");
+  console.log(splitS);
+  const newString = [];
+  // loop through splitS
+  for (let i = 0; i < splitS.length; i++) {
+    let addSpace = i % numRows;
+    if (i !== 0) {
+      if (addSpace === 0) {
+        newString.push("");
+        console.log("space", newString);
+      } else {
+        newString.push(splitS[i]);
+        console.log("letter", newString);
+      }
+    } else {
+      newString.push(splitS[i]);
+      console.log("letter", newString);
+    }
+  }
+  return newString;
+  /*    const newString = [];
+    for (let j = 0; j < numRows; j++) {
+      let position = [j, i];
+      newString.push(splitS[i + j]);
+      console.log(position, newString);
+    }
+    */
+};
+// change column when row > numRow * col + 1
+// read by row
+// remove blanks
+// return as string
+//};
+
+convert("PAYPALISHIRING", 3);
+/* Example 1:
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+Example 3:
+
+Input: s = "A", numRows = 1
+Output: "A"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of English letters (lower-case and upper-case), ',' and '.'.
+1 <= numRows <= 1000
+*/
