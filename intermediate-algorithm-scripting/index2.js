b766d4a2897d52d9e915c8e1bca59419fa00f9fe
Bernie Petters <bp4924@gmail.com>
2024-02-11 13:17:12 -0500

    build fib

--- intermediate-algorithm-scripting/index2.js
+++ intermediate-algorithm-scripting/index2.js
@@ -1,10 +1,19 @@
 // sum of odd fibs
 function sumFibs(num) {
   // create fib sequence
-  for (let i = 0; i <= num; i++) {}
+
+  let fibSeq = [];
+  for (let i = 0; i <= num; i++) {
+    if (i === 0) {
+      fibSeq.push("0");
+    } else {
+      fibSeq.push([i - 1] + [i - 2]);
+    }
+  }
+
   // exclude evens
   // sum result
-  return num;
+  return fibSeq;
 }
 
-sumFibs(4);
+console.log(sumFibs(4));
