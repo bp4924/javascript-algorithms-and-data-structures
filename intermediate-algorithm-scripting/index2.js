bff5d45deaaa691aab38f50b8ee2c1bdb7c97c70
Bernie Petters <bp4924@gmail.com>
2024-02-15 15:35:18 -0500

    sum the odds

--- intermediate-algorithm-scripting/index2.js
+++ intermediate-algorithm-scripting/index2.js
@@ -3,17 +3,27 @@ function sumFibs(num) {
   // create fib sequence
 
   let fibSeq = [];
+  let result = 0;
+  let fibNbr = 0;
   for (let i = 0; i <= num; i++) {
-    if (i === 0) {
-      fibSeq.push("0");
+    if (i === 0 || i === 1) {
+      fibSeq.push(i);
+      result += i;
     } else {
-      fibSeq.push([i - 1] + [i - 2]);
+      fibNbr = fibSeq[i - 1] + fibSeq[i - 2];
+      fibSeq.push(fibNbr);
+      if (fibNbr % 2 !== 0 && fibNbr <= num) {
+        result += fibNbr;
+      }
     }
   }
-
   // exclude evens
   // sum result
-  return fibSeq;
+  return result;
 }
 
+console.log(sumFibs(1000));
+console.log(sumFibs(4000000));
 console.log(sumFibs(4));
+console.log(sumFibs(75024));
+console.log(sumFibs(75025));
