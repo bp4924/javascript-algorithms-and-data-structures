cd06ecfe7d8cbcef94b543d1864766dbb8186209
Bernie Petters <bp4924@gmail.com>
2024-02-17 08:05:45 -0500

    sum of primes

--- intermediate-algorithm-scripting/index3.js
+++ intermediate-algorithm-scripting/index3.js
@@ -1,6 +1,18 @@
 // sunm all primes
 function sumPrimes(num) {
-  return num;
+  const primes = [2]; // initiate array of primes
+  let result = 2;
+  for (let n = 3; n <= num; n += 2) {
+    // check odd values
+    if (primes.every((prime) => n % prime !== 0)) {
+      // if n is prime, add to array and result
+      primes.push(n);
+      result += n;
+    }
+  }
+
+  return result;
 }
 
-sumPrimes(10);
+console.log(sumPrimes(10));
+console.log(sumPrimes(977));
