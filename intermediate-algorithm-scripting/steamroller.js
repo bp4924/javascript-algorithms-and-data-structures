7d43b8d05f22f6409c1807c8124988dbb4a9c522
Bernie Petters <bp4924@gmail.com>
2024-02-18 10:03:16 -0500

    reduce method

--- intermediate-algorithm-scripting/steamroller.js
+++ intermediate-algorithm-scripting/steamroller.js
@@ -1,5 +1,13 @@
 function steamrollArray(arr) {
-  return arr;
+  return arr.reduce((acc, curr) => {
+    if (Array.isArray(curr)) {
+      return acc.concat(steamrollArray(curr));
+    }
+    return acc.concat(curr);
+  }, []);
 }
 
-steamrollArray([1, [2], [3, [[4]]]]);
+console.log(steamrollArray([[["a"]], [["b"]]]));
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
+console.log(steamrollArray([1, [], [3, [[4]]]]));
+console.log(steamrollArray([1, {}, [3, [[4]]]]));
