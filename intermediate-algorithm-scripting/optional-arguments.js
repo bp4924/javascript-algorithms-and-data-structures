253c2abf2b9190476923ca2d7355fb0e0081bb13
Bernie Petters <bp4924@gmail.com>
2024-02-23 11:20:53 -0500

    return function

--- intermediate-algorithm-scripting/optional-arguments.js
+++ intermediate-algorithm-scripting/optional-arguments.js
@@ -1,5 +1,15 @@
 function addTogether() {
-  return false;
+  const [first, second] = arguments;
+
+  if (typeof first === "number") {
+    if (typeof second === "number") return first + second;
+    if (arguments.length === 1) return (second) => addTogether(first, second);
+  }
 }
 
-addTogether(2, 3);
+console.log(addTogether(2, 3));
+console.log(addTogether(23.5, 30));
+console.log(addTogether("2", 3));
+console.log(addTogether(5, undefined));
+console.log(addTogether(5));
+console.log(addTogether(5)(7));
