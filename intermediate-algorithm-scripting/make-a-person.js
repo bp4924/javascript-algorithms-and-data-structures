e7d829da2564047fe21de511f01f101012d4ab99
Bernie Petters <bp4924@gmail.com>
2024-02-27 07:17:46 -0500

    create class

--- intermediate-algorithm-scripting/make-a-person.js
+++ intermediate-algorithm-scripting/make-a-person.js
@@ -1,36 +1,41 @@
-const Person = function (first, last) {
-  let firstName = first;
-  let lastName = last;
+class Person {
+  constructor(first, last) {
+    let firstName = first;
+    let lastName = last;
 
-  this.getFirstName = () => {
-    return firstName;
-  };
+    this.getFirstName = () => {
+      return firstName;
+    };
 
-  this.getLastName = () => {
-    return lastName;
-  };
+    this.getLastName = () => {
+      return lastName;
+    };
 
-  this.getFullName = function () {
-    return this.getFirstName() + " " + this.getLastName();
-  };
+    this.getFullName = function () {
+      return this.getFirstName() + " " + this.getLastName();
+    };
 
-  this.setFirstName = (first) => {
-    return (firstName = first);
-  };
+    this.setFirstName = (first) => {
+      return (firstName = first);
+    };
 
-  this.setLastName = (last) => {
-    return (lastName = last);
-  };
+    this.setLastName = (last) => {
+      return (lastName = last);
+    };
 
-  this.setFullName = (first, last) => {
-    this.setFirstName(first);
-    this.setLastName(last);
-    return this.getFullName();
-  };
-};
+    this.setFullName = (first, last) => {
+      this.setFirstName(first);
+      this.setLastName(last);
+      return this.getFullName();
+    };
+  }
+}
 
 const bob = new Person("Bob", "Ross");
 console.log(bob.getFullName());
 
 const haskell = new Person("Haskell", "Ross");
 console.log(haskell.setFirstName("Haskell"));
+
+const john = new Person("John", "Doe");
+console.log(john.getFullName());
