6b8a9ea5e497d7b1c6cf3e41e98cb8be9e696386
Bernie Petters <bp4924@gmail.com>
2024-03-01 19:05:13 -0500

    loop

---
+++ intermediate-algorithm-scripting/roman-numerals.js
@@ -0,0 +1,12 @@
+// loop through the integer by position
+// for int[0]
+const int = 3999;
+const str = int.toString();
+for (let i = 0; i <= str.length - 1; i++) {
+  console.log(i + " " + str[i]);
+  let position = str.length - i;
+  // find value at position
+  // replace with roman numeral
+  // create string of roman numerals
+  console.log(position);
+}
