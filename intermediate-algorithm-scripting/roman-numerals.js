86a2c821eba959843f0aa7b88a6bb30692a935ec
Bernie Petters <bp4924@gmail.com>
2024-03-02 09:24:51 -0500

    switch statement

--- intermediate-algorithm-scripting/roman-numerals.js
+++ intermediate-algorithm-scripting/roman-numerals.js
@@ -1,12 +1,33 @@
 // loop through the integer by position
 // for int[0]
+
 const int = 3999;
 const str = int.toString();
+let result = "";
 for (let i = 0; i <= str.length - 1; i++) {
-  console.log(i + " " + str[i]);
   let position = str.length - i;
+  switch (position) {
+    case 4:
+      console.log("I");
+      result = "I" + result;
+      break;
+    case 3:
+      console.log("X");
+      result = "X" + result;
+      break;
+    case 2:
+      console.log("C");
+      result = "C" + result;
+      break;
+    case 1:
+      console.log("M");
+      result = "M" + result;
+      break;
+  }
+
   // find value at position
   // replace with roman numeral
   // create string of roman numerals
-  console.log(position);
+  // console.log(result);
 }
+console.log(result);
