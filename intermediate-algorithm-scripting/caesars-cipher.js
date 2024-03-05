81a9f9892d454bfa0c53a74cab09def9260bb022
Bernie Petters <bp4924@gmail.com>
2024-03-05 11:17:58 -0500

    char code methods (to and from)

--- intermediate-algorithm-scripting/caesars-cipher.js
+++ intermediate-algorithm-scripting/caesars-cipher.js
@@ -1,7 +1,33 @@
 // returm a string off-set by 13 characters
 
 function rot13(str) {
-  return str;
+  const step = 13;
+  const zValue = 90;
+  const aValue = 65;
+  let result = "";
+
+  // loop throught string
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    // convert letters to numbers
+    let code = char.charCodeAt();
+
+    if (code >= aValue && code <= zValue) {
+      // add 13 to number
+      code = code + step;
+      if (code > zValue) {
+        code = code - 26;
+      }
+      // convert number to character.
+      char = String.fromCharCode(code);
+    }
+    // concat to result
+    result = result + char;
+  }
+  return result;
 }
 
-rot13("SERR PBQR PNZC");
+console.log(rot13("SERR PBQR PNZC")); // FREE CODE CAMP
+console.log(rot13("SERR CVMMN!")); // FREE PIZZA!
+console.log(rot13("SERR YBIR?")); // FREE LOVE?
+console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.")); //THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
