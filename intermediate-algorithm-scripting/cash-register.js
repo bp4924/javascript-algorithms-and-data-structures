16518081bfb20ca8172aee233fc8a3d9b342b59d
Bernie Petters <bp4924@gmail.com>
2024-03-13 11:24:54 -0400

    status closed

--- intermediate-algorithm-scripting/cash-register.js
+++ intermediate-algorithm-scripting/cash-register.js
@@ -1,31 +1,23 @@
 function checkCashRegister(price, cash, cid) {
+  const startCID = cid;
   let changeDue = 100 * (cash - price);
   const cidValues = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000];
   let change = [];
-  /* 
+
+  /*
   let cidTotal = 0;
   for (let i = 0; i < cid.length; i++) {
     cidTotal += cid[i][1];
   }
   console.log(cidTotal);
- */
+*/
+
   let status = "OPEN";
-
-  // increment change
-  // if change > == && cid == 0, insufficient funds
-  // else return change
-
-  /*   // check for sufficient change
-  if (changeDue === 0) {
-    status = "CLOSED";
-    change = cid;
-  }
- */
 
   for (let i = cid.length - 1; i >= 0; i--) {
     // remove change from cid until change == 0 or cid == 0
-    console.log(`i ${i} `);
-    console.log(`cidValues ${cidValues[i]} changeDue ${changeDue}`);
+    // console.log(`i ${i} `);
+    // console.log(`cidValues ${cidValues[i]} changeDue ${changeDue}`);
     let changeGiven = 0;
 
     while (cidValues[i] <= changeDue) {
@@ -33,26 +25,30 @@ function checkCashRegister(price, cash, cid) {
 
       //      console.log(`changeDue ${changeDue}`);
 
-      console.log(`cid[i] ${cid[i]}`);
-      console.log(`cidValues[i] ${cidValues[i]}`);
+      // console.log(`cid[i] ${cid[i]}`);
+      // console.log(`cidValues[i] ${cidValues[i]}`);
       if (cid[i][1] === 0) break;
 
       cid[i][1] -= cidValues[i] / 100;
-      console.log(`NEW cid[i][1] ${cid[i][1]}`);
+      // console.log(`NEW cid[i][1] ${cid[i][1]}`);
 
-      changeGiven += cidValues[i] / 100;
+      changeGiven += cidValues[i];
       changeDue -= cidValues[i];
-      console.log(` NEW changeDue ${changeDue}`);
-
-      // push to change
+      // console.log(` NEW changeDue ${changeDue}`);
+      if (changeDue === cidValues[i - 1]) {
+        status = "CLOSED";
+        change = startCID;
+      }
     }
+    console.log(` NEW changeDue ${changeDue}`);
 
-    // TODO: stop taking change when drawer is < change
-
-    cid[i][1] = changeGiven;
+    cid[i][1] = changeGiven / 100;
+    console.log(`change given ${changeGiven}`);
+    // push to change
     if (changeGiven > 0) {
       change.push(cid[i]);
       //      console.log(`change ${change}`);
+      // TODO: return Closed for exact change
     }
   }
   if (changeDue > 0) {
