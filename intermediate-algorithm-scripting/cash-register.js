78cde763018fd698388a879658bddc3d7a90037b
Bernie Petters <bp4924@gmail.com>
2024-03-17 07:25:41 -0400

    exact change use case

--- intermediate-algorithm-scripting/cash-register.js
+++ intermediate-algorithm-scripting/cash-register.js
@@ -1,57 +1,40 @@
 function checkCashRegister(price, cash, cid) {
   const startCID = cid;
-  let changeDue = 100 * (cash - price);
+  let startChangeDue = 100 * (cash - price);
+  let changeDue = startChangeDue;
   const cidValues = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000];
   let change = [];
 
-  /*
-  let cidTotal = 0;
-  for (let i = 0; i < cid.length; i++) {
-    cidTotal += cid[i][1];
-  }
-  console.log(cidTotal);
-*/
-
   let status = "OPEN";
-
+  let changeGiven = 0;
   for (let i = cid.length - 1; i >= 0; i--) {
     // remove change from cid until change == 0 or cid == 0
-    // console.log(`i ${i} `);
-    // console.log(`cidValues ${cidValues[i]} changeDue ${changeDue}`);
-    let changeGiven = 0;
+    changeGiven = 0;
 
     while (cidValues[i] <= changeDue) {
       // get change from drawer
-
-      //      console.log(`changeDue ${changeDue}`);
-
-      // console.log(`cid[i] ${cid[i]}`);
-      // console.log(`cidValues[i] ${cidValues[i]}`);
       if (cid[i][1] === 0) break;
 
       cid[i][1] -= cidValues[i] / 100;
-      // console.log(`NEW cid[i][1] ${cid[i][1]}`);
 
       changeGiven += cidValues[i];
       changeDue -= cidValues[i];
-      // console.log(` NEW changeDue ${changeDue}`);
     }
-    console.log(` NEW changeDue ${changeDue}`);
 
     cid[i][1] = changeGiven / 100;
-    console.log(`change given ${changeGiven}`);
     // push to change
     if (changeGiven > 0) {
       change.push(cid[i]);
-      // console.log(`change ${change}`);
-
-      // TODO: return Closed for exact change
-    }
-    if (changeDue === cidValues[i]) {
-      status = "CLOSED";
-      change = startCID;
     }
   }
+  //  console.log(`change given ${changeGiven} cash ${cash} startCID ${startCID}`);
+  // return Closed for exact change
+
+  if (changeGiven === startChangeDue) {
+    status = "CLOSED";
+    change = startCID;
+  }
+
   if (changeDue > 0) {
     status = "INSUFFICIENT_FUNDS";
     change = [];
