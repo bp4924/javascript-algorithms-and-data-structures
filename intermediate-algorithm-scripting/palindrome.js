25e6d5c4a4e14ace30c6040e2b62a0f2a104abe9
Bernie Petters <bp4924@gmail.com>
2024-03-17 17:12:50 -0400

    initial condition

--- intermediate-algorithm-scripting/palindrome.js
+++ intermediate-algorithm-scripting/palindrome.js
@@ -1,4 +1,15 @@
 function palindrome(str) {
+  // strip non-letter characters
+
+  //const arrStr = str.split("");
+  //  console.log(arrStr);
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] != str[str.length - i - 1]) {
+      return false;
+    }
+  }
+
+  //  console.log(reverseStr);
   return true;
 }
 
